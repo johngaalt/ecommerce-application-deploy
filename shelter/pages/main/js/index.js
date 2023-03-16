1ed2d542ce035962ee68a73a5b62e83452481f6d
johngaalt <rajon777666@gmail.com>
2023-03-16 15:58:17 +0300

    feat: component start-screen main

--- shelter/pages/main/js/index.js
+++ shelter/pages/main/js/index.js
@@ -1,7 +1,8 @@
 import "../styles/style.scss";
 import footer from "../../../components/footer/footer.html";
 import header from "../../../components/header/header.html";
+import startScreen from "../../../components/start-screen/start-screen.html";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.innerHTML = header + footer;
+  document.body.innerHTML = startScreen + header + footer;
 });
