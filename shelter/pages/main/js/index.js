b5aaf473e7033d0c61a30202a02ee671468c6bb0
johngaalt <rajon777666@gmail.com>
2023-03-16 21:13:51 +0300

    feat: styles about section

--- shelter/pages/main/js/index.js
+++ shelter/pages/main/js/index.js
@@ -1,7 +1,8 @@
 import "../styles/style.scss";
 import footer from "../../../components/footer/footer.html";
 import startScreen from "../../../components/start-screen/start-screen.js";
+import about from "../../../components/about/about.html";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.innerHTML = startScreen + footer;
+  document.body.innerHTML = startScreen + about + footer;
 });
