15a826a7916c6c2c643831e48519b2733c9d708f
johngaalt <rajon777666@gmail.com>
2023-03-17 11:15:16 +0300

    feat: pets, pets-cards files

--- shelter/pages/main/js/index.js
+++ shelter/pages/main/js/index.js
@@ -2,7 +2,8 @@ import "../styles/style.scss";
 import footer from "../../../components/footer/footer.html";
 import startScreen from "../../../components/start-screen/start-screen.js";
 import about from "../../../components/about/about.html";
+import pets from "../../../components/pets/pets.html";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.innerHTML = startScreen + about + footer;
+  document.body.innerHTML = startScreen + about + pets + footer;
 });
