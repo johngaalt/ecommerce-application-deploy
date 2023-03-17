02742b9432e98f667571e175732b4321510c9002
johngaalt <rajon777666@gmail.com>
2023-03-17 15:24:56 +0300

    feat: basic layout help

--- shelter/pages/main/js/index.js
+++ shelter/pages/main/js/index.js
@@ -4,7 +4,9 @@ import startScreen from "../../../components/start-screen/start-screen.js";
 import about from "../../../components/about/about.html";
 import pets from "../../../components/pets/pets.html";
 import petCard from "../../../components/pet-card/pet-card.html";
+import help from "../../../components/help/help.html";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.innerHTML = startScreen + about + pets + petCard + footer;
+  document.body.innerHTML =
+    startScreen + about + pets + petCard + help + footer;
 });
