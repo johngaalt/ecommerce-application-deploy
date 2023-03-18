a89098045891a1ca0a7a191d5c66218ebcbc5f2d
johngaalt <rajon777666@gmail.com>
2023-03-18 12:12:18 +0300

    refactor: startScreen header

--- shelter/components/start-screen/start-screen.js
+++ shelter/components/start-screen/start-screen.js
@@ -1,9 +1,5 @@
 import header from "../header/header.html";
-import { createElementFromString } from "../utils/create-element-from-string";
 import startScreen from "./start-screen.html";
 
-const startScreenElem = createElementFromString(startScreen);
-const headerDiv = startScreenElem.querySelector("#header");
-headerDiv.innerHTML = header;
-
-export default startScreenElem.innerHTML;
+const startScreenWithHeader = startScreen.replace("#HEADER#", header);
+export default startScreenWithHeader;
