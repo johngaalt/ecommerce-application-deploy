0ba451ba9df31dab93b515290b671499d57fca35
johngaalt <rajon777666@gmail.com>
2023-03-18 12:50:52 +0300

    feat: pets page

--- shelter/pages/pets/js/index.js
+++ shelter/pages/pets/js/index.js
@@ -1,7 +1,10 @@
 import "../styles/style.scss";
+// html
 import footer from "../../../components/footer/footer.html";
 import header from "../../../components/header/header.html";
+// js
+import pets from "../../../components/pets/pets.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.innerHTML = header + footer;
+  document.body.innerHTML = header + pets + footer;
 });
