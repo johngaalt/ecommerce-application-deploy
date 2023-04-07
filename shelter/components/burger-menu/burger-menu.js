0b8cfce790cd0a6547a83abc7a757b67022a4942
johngaalt <rajon777666@gmail.com>
2023-04-07 15:13:57 +0300

    feat: styles burger, overlay, outsideClick

--- shelter/components/burger-menu/burger-menu.js
+++ shelter/components/burger-menu/burger-menu.js
@@ -6,10 +6,23 @@ export function addEventListenerBurgerMenu() {
       const nav = document.querySelector(".nav");
       const links = document.querySelectorAll(".list__item");
       const body = document.body;
+      let overlay = document.querySelector(".overlay");
 
       nav.classList.toggle("nav_active");
       burger.classList.toggle("burger_active");
       body.classList.toggle("body_hidden");
+
+      if (nav.classList.contains("nav_active")) {
+        overlay = document.createElement("div");
+        overlay.classList.add("overlay");
+        body.appendChild(overlay);
+        overlay.classList.add("overlay_active");
+        document.addEventListener("click", handleOutsideClick);
+      } else if (overlay) {
+        body.removeChild(overlay);
+        overlay.classList.remove("overlay_active");
+        document.removeEventListener("click", handleOutsideClick);
+      }
 
       links.forEach((link) => {
         link.addEventListener("click", (event) => {
@@ -17,9 +30,36 @@ export function addEventListenerBurgerMenu() {
             nav.classList.remove("nav_active");
             burger.classList.remove("burger_active");
             body.classList.remove("body_hidden");
+            overlay = document.querySelector(".overlay");
+            if (overlay) {
+              body.removeChild(overlay);
+              overlay.classList.remove("overlay_active");
+              document.removeEventListener("click", handleOutsideClick);
+            }
           }
         });
       });
+    }
+  }
+
+  function handleOutsideClick(event) {
+    const nav = document.querySelector(".nav");
+    const burger = document.querySelector(".burger");
+
+    if (
+      !event.target.closest(".nav") &&
+      !event.target.closest(".burger") &&
+      nav.classList.contains("nav_active")
+    ) {
+      nav.classList.remove("nav_active");
+      burger.classList.remove("burger_active");
+      document.body.classList.remove("body_hidden");
+      const overlay = document.querySelector(".overlay");
+      if (overlay) {
+        document.body.removeChild(overlay);
+        overlay.classList.remove("overlay_active");
+      }
+      document.removeEventListener("click", handleOutsideClick);
     }
   }
 
