283c7f7486e1c3abf52c5c346aefb857904de650
johngaalt <rajon777666@gmail.com>
2023-04-09 11:36:26 +0300

    feat: modal window

--- shelter/components/pets/pets.js
+++ shelter/components/pets/pets.js
@@ -3,29 +3,21 @@ import pets from "./pets.html";
 import jennifer from "../../assets/images/pets-jennifer.png";
 import katrine from "../../assets/images/pets-katrine.png";
 import woody from "../../assets/images/pets-woody.png";
+import sophia from "../../assets/images/pets-sophia.png";
+import timmy from "../../assets/images/pets-timmy.png";
+import charley from "../../assets/images/pets-charly.png";
+import scarlett from "../../assets/images/pets-scarlet.png";
+import freddie from "../../assets/images/pets-freddie.png";
 
 const petsArr = [
   { imgSrc: katrine, name: "Katrine" },
   { imgSrc: jennifer, name: "Jennifer" },
   { imgSrc: woody, name: "Woody" },
-  { imgSrc: katrine, name: "Katrine" },
-  { imgSrc: jennifer, name: "Jennifer" },
-  { imgSrc: woody, name: "Woody" },
-  { imgSrc: katrine, name: "Katrine" },
-  { imgSrc: jennifer, name: "Jennifer" },
-  { imgSrc: woody, name: "Woody" },
-  { imgSrc: katrine, name: "Katrine" },
-  { imgSrc: jennifer, name: "Jennifer" },
-  { imgSrc: woody, name: "Woody" },
-  { imgSrc: katrine, name: "Katrine" },
-  { imgSrc: jennifer, name: "Jennifer" },
-  { imgSrc: woody, name: "Woody" },
-  { imgSrc: katrine, name: "Katrine" },
-  { imgSrc: jennifer, name: "Jennifer" },
-  { imgSrc: woody, name: "Woody" },
-  { imgSrc: katrine, name: "Katrine" },
-  { imgSrc: jennifer, name: "Jennifer" },
-  { imgSrc: woody, name: "Woody" },
+  { imgSrc: sophia, name: "Sophia" },
+  { imgSrc: timmy, name: "Timmy" },
+  { imgSrc: charley, name: "Charly" },
+  { imgSrc: scarlett, name: "Scarlett" },
+  { imgSrc: freddie, name: "Freddie" },
 ];
 
 const petsElements = petsArr
--- shelter/pages/main/js/index.js
+++ shelter/pages/main/js/index.js
@@ -4,6 +4,7 @@ import "../styles/style.scss";
 import footer from "../../../components/footer/footer.html";
 import about from "../../../components/about/about.html";
 import donation from "../../../components/donation/donation.html";
+import popUp from "../../../components/pop-up/pop-up.html";
 //  js
 import help from "../../../components/help/help.js";
 import startScreen from "../../../components/start-screen/start-screen.js";
@@ -14,7 +15,7 @@ import { runSlider } from "../../../components/slider/slider";
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.innerHTML =
-    startScreen + about + pets + help + donation + footer;
+    startScreen + about + pets + help + donation + popUp + footer;
 
   toggleActiveLinkNavigation();
   addEventListenerBurgerMenu();
