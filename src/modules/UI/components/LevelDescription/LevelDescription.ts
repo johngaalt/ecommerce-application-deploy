c685f3295f03f7681c838a82ca9e437da8be71b6
johngaalt <rajon777666@gmail.com>
2023-06-16 14:21:28 +0300

    refactor(level-description): rename elements and placeholders

--- src/modules/UI/components/LevelDescription/LevelDescription.ts
+++ src/modules/UI/components/LevelDescription/LevelDescription.ts
@@ -1,6 +1,21 @@
 import levelDescription from './LevelDescription.html';
 
 export class LevelDescription {
+  private LEVEL_DESCRIPTION_ID = 'level-description';
+
+  show() {
+    const levelDescriptionEl = document.getElementById(
+      this.LEVEL_DESCRIPTION_ID
+    );
+    levelDescriptionEl?.classList.remove('d-none');
+  }
+
+  hide() {
+    const levelDescriptionEl = document.getElementById(
+      this.LEVEL_DESCRIPTION_ID
+    );
+    levelDescriptionEl?.classList.add('d-none');
+  }
   render() {
     return levelDescription;
   }
--- src/modules/UI/components/Sidebar/Sidebar.ts
+++ src/modules/UI/components/Sidebar/Sidebar.ts
@@ -6,7 +6,6 @@ export class Sidebar {
   private levelDescription: LevelDescription;
   private levelList: LevelList;
   private BURGER_ID = 'burger';
-  private HELP_ID = 'level-description';
   private CROSS_ID = 'cross';
 
   constructor() {
@@ -18,7 +17,7 @@ export class Sidebar {
     const burgerEl = document.getElementById(this.BURGER_ID);
 
     burgerEl?.addEventListener('click', () => {
-      this.hideHelp();
+      this.levelDescription.hide();
       this.levelList.show();
     });
   }
@@ -27,24 +26,14 @@ export class Sidebar {
     const crossEl = document.getElementById(this.CROSS_ID);
 
     crossEl?.addEventListener('click', () => {
-      this.showHelp();
+      this.levelDescription.show();
       this.levelList.hide();
     });
   }
 
-  showHelp() {
-    const helpEl = document.getElementById(this.HELP_ID);
-    helpEl?.classList.remove('d-none');
-  }
-
-  hideHelp() {
-    const helpEl = document.getElementById(this.HELP_ID);
-    helpEl?.classList.add('d-none');
-  }
-
   render() {
     return sidebar
-      .replace('#DISPLAYHELP#', this.levelDescription.render())
-      .replace('#LEVELDESCRIPTION#', this.levelList.render());
+      .replace('#LEVELDESCRIPTION#', this.levelDescription.render())
+      .replace('#LEVELLIST#', this.levelList.render());
   }
 }
