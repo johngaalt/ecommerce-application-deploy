a7b594f46d24bd9b845e35275b6905ca2bf1855e
johngaalt <rajon777666@gmail.com>
2023-06-20 10:17:38 +0300

    feat: levels.ts, table styles

---
+++ src/modules/GameState/types/ILevel.ts
@@ -0,0 +1,10 @@
+export interface ILevel {
+  helpTitle?: string;
+  selectorName?: string;
+  doThis: string;
+  selector: string;
+  syntax: string;
+  help?: string;
+  examples?: string[];
+  boardMarkup: string;
+}
