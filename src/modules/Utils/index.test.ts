f888f57860b627cfbd6681c5bd46bab6bd4da331
johngaalt <rajon777666@gmail.com>
2023-07-02 13:23:49 +0300

    test(Utils): should parse html string to unicode representation

--- src/modules/Utils/index.test.ts
+++ src/modules/Utils/index.test.ts
@@ -8,4 +8,14 @@ describe('Utils', () => {
       expect(node).toBeInstanceOf(Node);
     });
   });
+
+  describe('parseMarkup', () => {
+    it('should parse html string to unicode representation', () => {
+      const markup = Utils.parseMarkup('<div></div>');
+
+      expect(markup).toBe(`&lt;div class=&quot;table&quot;&gt;
+  &lt;div&gt;&lt;/div&gt;
+&lt;/div&gt;`);
+    });
+  });
 });
