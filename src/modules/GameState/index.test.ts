812c950fb450d5a98a8e6294ff33e1dc698b3737
johngaalt <rajon777666@gmail.com>
2023-07-02 12:00:28 +0300

    test(GameState): should subscribe to event bus

--- src/modules/GameState/index.test.ts
+++ src/modules/GameState/index.test.ts
@@ -1,13 +1,22 @@
+import eventBus from 'eventBus/index';
 import { GameState } from '.';
 
 describe('GameState', () => {
-  const gameState = new GameState();
-
   it('should be initialized with default values', () => {
+    const gameState = new GameState();
+
     expect(gameState).toEqual({
       currentLevelId: 1,
       tippedLevels: new Set(),
       _finishedLevels: {},
     });
+  });
+
+  it('should subscribe to event bus', () => {
+    const subscribeSpy = jest.spyOn(eventBus, 'subscribe');
+
+    new GameState();
+
+    expect(subscribeSpy).toHaveBeenCalled();
   });
 });
